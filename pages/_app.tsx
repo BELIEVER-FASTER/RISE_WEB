b8f1d46eb21738cf49f6350c36df9f246c6b4185
conrad <yhg0337@naver.com>
2021-08-11 16:50:38 +0900

    Add google-tag-manager
    - update open-graph image
    - add @react-gtm-module
    - adapt google-tag-manager

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,17 @@
 import { AppProps } from 'next/dist/next-server/lib/router/router';
 import { GlobalStyle } from 'utils/style_opt';
+import TagManager from 'react-gtm-module';
+import { useEffect } from 'react';
+
+const tagManagerArgs = {
+  gtmId: process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID as string,
+};
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  useEffect(() => {
+    TagManager.initialize(tagManagerArgs);
+  }, []);
+
   return (
     <>
       <Component {...pageProps} />
